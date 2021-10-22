63eab10a3b0f7dae08aea6b00fbacaf192eafa31
kolmus <lukasz.kolmus@gmail.com>
2021-10-22 15:28:40 +0200

    Guesing numbers - second verion of this game - code works ok

---
+++ game_v2.py
@@ -0,0 +1,32 @@
+print('Pomyśl liczbę od 0 do 1000, a ja ją zgadnę w max. 10 próbach!')
+
+min_num = 0
+max_num = 1000
+test = 0 #  made for check if choise is corect
+
+for i in range(10):
+    guess = int((max_num - min_num) / 2) + min_num
+    while True:
+        if i + 1 == 10:
+            print(f'Twoja liczba to {guess}!!!')
+            break
+        try:
+            print(f'\n Próba {i + 1},\n\n Zgaduję: {guess}')
+            answer = input('\nWpisz cyfrę odpowiedzi:\n  1  Za dużo\n  2  Za mało\n  3  Zgadłeś\n\n  ')
+            test = int(answer)
+            if answer == '1':
+                max_num = guess
+                break
+            elif answer == '2':
+                min_num = guess
+                break
+            elif answer == '3':
+                print('Wygrałem!\n')
+                break
+            else:
+                print('Nie oszukuj!')
+                continue
+
+        except ValueError:
+            print('\nTo nie jest poprawny wybór! spróbuj jeszcze raz\n')
+            continue
